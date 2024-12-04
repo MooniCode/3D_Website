b3f5fcdc2d2571e3162feabb3bf362a461fdcc08
Immanuel Claerhout <immanuel.claerhout999@gmail.com>
2024-12-04 16:20:58 +0100

    fixed three issue

--- jsFiles/aboutus.js
+++ jsFiles/aboutus.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'https://unpkg.com/three@latest/build/three.module.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 // Scene setup
 const scene = new THREE.Scene();
